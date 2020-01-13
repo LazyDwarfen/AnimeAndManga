a4d9c9374bc56036dae883d4dbcc63a7ed2e3ac8
Эмиль <Wolker97@gmail.com>
2020-01-13 04:27:49 +0400

    Update Configuration.java

--- src/main/java/animeAndManga/config/Configuration.java
+++ src/main/java/animeAndManga/config/Configuration.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @org.springframework.context.annotation.Configuration
-public class Configuration implements WebMvcConfigurer {
+public class  Configuration implements WebMvcConfigurer {
    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/", "classpath:/resources/",
             "classpath:/static/", "classpath:/public/" };
